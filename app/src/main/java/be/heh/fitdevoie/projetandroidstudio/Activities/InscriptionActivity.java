4a95d9a90c82e36c682cd199502d92bbf1597f81
LouisFitdevoie <fitdevoielouis@hotmail.be>
2021-12-09 14:56:40 +0100

    Implémentation vérification des champs de la page d'inscription

--- app/src/main/java/be/heh/fitdevoie/projetandroidstudio/Activities/InscriptionActivity.java
+++ app/src/main/java/be/heh/fitdevoie/projetandroidstudio/Activities/InscriptionActivity.java
@@ -1,10 +1,17 @@
 package be.heh.fitdevoie.projetandroidstudio.Activities;
 
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,6 +30,12 @@ public class InscriptionActivity extends AppCompatActivity {
     EditText et_inscription_password;
     EditText et_inscription_passwordConfirmation;
     TextView tv_inscription_incomplete;
+    Button bt_inscription;
+
+    boolean firstName_ok = false;
+    boolean lastName_ok = false;
+    boolean email_ok = false;
+    boolean pwd_ok = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +50,7 @@ public class InscriptionActivity extends AppCompatActivity {
         et_inscription_password = (EditText) findViewById(R.id.et_inscription_password);
         et_inscription_passwordConfirmation = (EditText) findViewById(R.id.et_inscription_passwordConfirmation);
         tv_inscription_incomplete = (TextView) findViewById(R.id.tv_inscription_incomplete);
+        bt_inscription = (Button) findViewById(R.id.bt_inscription);
     }
 
     public void onInscriptionClickManager(View v) {
@@ -44,40 +58,72 @@ public class InscriptionActivity extends AppCompatActivity {
         final int BT_HOME = R.id.home;
         switch(v.getId()) {
             case BT_INSCRIPTION:
-                System.out.println("Click sur inscription");
                 tv_inscription_incomplete.setText("");
-                String inscription_incomplete_description = "Veuillez corriger/remplir les champs suivants :\n";
-                Boolean pwd_ok = false;
+                String inscription_incomplete_description = "Veuillez corriger/remplir les champs suivants :";
+
                 try {
+
+                    if(et_inscription_firstName.getText().toString().trim().length() == 0) {
+                        this.firstName_ok = false;
+                        inscription_incomplete_description += "\n- Prénom";
+                    } else {
+                        this.firstName_ok = true;
+                    }
+
+                    if(et_inscription_lastName.getText().toString().trim().length() == 0) {
+                        this.lastName_ok = false;
+                        inscription_incomplete_description += "\n- Nom de famille";
+                    } else {
+                        this.lastName_ok = true;
+                    }
+
+                    if(et_inscription_email.getText().toString().trim().length() == 0) {
+                        this.email_ok = false;
+                        inscription_incomplete_description += "\n- Adresse email";
+                    } else {
+                        this.email_ok = true;
+                    }
+
                     if(et_inscription_password.getText().toString().trim().length() == 0 || et_inscription_passwordConfirmation.getText().toString().trim().length() == 0) {
-                        pwd_ok = false;
+                        this.pwd_ok = false;
                         if(et_inscription_password.getText().toString().trim().length() == 0) {
-                            inscription_incomplete_description += "- Mot de passe\n";
+                            inscription_incomplete_description += "\n- Mot de passe";
                         }
                         if(et_inscription_passwordConfirmation.getText().toString().trim().length() == 0) {
-                            inscription_incomplete_description += "- Confirmation du mot de passe\n";
+                            inscription_incomplete_description += "\n- Confirmation du mot de passe";
                         }
                     } else {
                         if(et_inscription_password.getText().toString().equals(et_inscription_passwordConfirmation.getText().toString())) {
-                            System.out.println("pwd ok");
-                            tv_inscription_incomplete.setVisibility(View.INVISIBLE);
-                            pwd_ok = true;
+                            this.pwd_ok = true;
                         } else {
-                            System.out.println("pwd pas ok");
-                            tv_inscription_incomplete.setVisibility(View.VISIBLE);
-                            pwd_ok = false;
+                            inscription_incomplete_description += "\n- Les mots de passe entrés sont différents";
+                            this.pwd_ok = false;
                         }
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                System.out.println("FN : " + this.firstName_ok + " LN : " + this.lastName_ok + " MAIL : " + this.email_ok + " PWD : " + this.pwd_ok);
+                if(this.firstName_ok && this.lastName_ok && this.email_ok && this.pwd_ok) {
+                    tv_inscription_incomplete.setText("");
+                    tv_inscription_incomplete.setVisibility(View.INVISIBLE);
+                    System.out.println(inscription_incomplete_description);
+                    //User user1 = new User(et_inscription_firstName.getText().toString(),et_inscription_lastName.getText().toString(),et_inscription_email.getText().toString(),et_inscription_password.getText().toString());
+                    UserAccessDB userDB = new UserAccessDB(this);
+                    userDB.openForWrite();
+                    //userDB.insertUser(user1);
+                    userDB.Close();
 
-                //User user1 = new User(et_inscription_firstName.getText().toString(),et_inscription_lastName.getText().toString(),et_inscription_email.getText().toString(),et_inscription_password.getText().toString());
+                    Intent toTest = new Intent(this, MainActivity.class);
+                    startActivity(toTest);
+                    finish();
+                } else {
 
-                UserAccessDB userDB = new UserAccessDB(this);
-                userDB.openForWrite();
-                //userDB.insertUser(user1);
-                userDB.Close();
+                    tv_inscription_incomplete.setText(inscription_incomplete_description);
+                    tv_inscription_incomplete.setMaxHeight(600);
+                    tv_inscription_incomplete.setVisibility(View.VISIBLE);
+                }
+
                 break;
         }
     }
