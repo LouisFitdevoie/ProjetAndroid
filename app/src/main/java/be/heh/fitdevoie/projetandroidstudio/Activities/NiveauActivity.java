7f7a88a7fa82479902f9a7390b1bc2ad14334d15
LouisFitdevoie <fitdevoielouis@hotmail.be>
2021-12-20 14:17:48 +0100

    Ajout de l'activité d'avertissement de niveau de liquide

---
+++ app/src/main/java/be/heh/fitdevoie/projetandroidstudio/Activities/NiveauActivity.java
@@ -0,0 +1,16 @@
+package be.heh.fitdevoie.projetandroidstudio.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import be.heh.fitdevoie.projetandroidstudio.R;
+
+public class NiveauActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_niveau);
+    }
+}
