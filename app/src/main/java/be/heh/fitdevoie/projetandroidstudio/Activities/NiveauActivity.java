de5bbfa37dc3c36f444ac3e2faef96884553a81d
LouisFitdevoie <fitdevoielouis@hotmail.be>
2021-12-24 15:28:24 +0100

    Modification du layout de l'activity_niveau

--- app/src/main/java/be/heh/fitdevoie/projetandroidstudio/Activities/NiveauActivity.java
+++ app/src/main/java/be/heh/fitdevoie/projetandroidstudio/Activities/NiveauActivity.java
@@ -3,10 +3,21 @@ package be.heh.fitdevoie.projetandroidstudio.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import be.heh.fitdevoie.projetandroidstudio.R;
 
 public class NiveauActivity extends AppCompatActivity {
+
+    Button bt_comprimes;
+    RelativeLayout rl_niveau_parametres;
+    EditText et_niveau_ip;
+    EditText et_niveau_rack;
+    EditText et_niveau_slot;
+    TextView tv_niveau_parametres_errorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
